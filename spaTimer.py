6d16eafeb01ff511e8a86f586cf09d05de399a7f
Michael Saunders <msaun@relational.com.au>
2019-09-11 00:04:11 +0930

    Damn timer stopped working on the arlec device. So we'll make our own...

---
+++ spaTimer.py
@@ -0,0 +1,46 @@
+import binascii
+import time
+import sys
+from orvibo import Orvibo
+
+SPA_MAC="DEVICE_MAC_HERE"
+ATTEMPTS_TO_TRY=120
+WAIT_SECONDS_BETWEEN_ATTEMPTS=30
+
+if len(sys.argv) != 2:
+  sys.exit('Incorrect number of arguments, please specify only ON or OFF')
+
+if ("ON" != sys.argv[1] and "OFF" != sys.argv[1]):
+  sys.exit('Please specify ON or OFF')
+
+switchToOn = (sys.argv[1] == "ON")
+
+def getIPForMac(mac):
+  for vals in Orvibo.discover().values():
+    if(binascii.hexlify(vals[1]) == mac):
+      print('Found IP=', vals[0])
+      return vals[0]
+  return None
+
+def getDeviceForMac(mac, attempts, delay):
+  attempt = 1
+  while attempt <= attempts:
+    ip = getIPForMac(mac)
+    if ip is not None:
+      return Orvibo(ip)
+    print('Waiting {}'.format(delay))
+    time.sleep(delay)
+  else:
+    print('Exhausted attempts to find mac: ', mac)
+  return None
+
+spaSocket = getDeviceForMac(SPA_MAC, ATTEMPTS_TO_TRY, WAIT_SECONDS_BETWEEN_ATTEMPTS)
+print('Spa power state: {}'.format(spaSocket.on))
+if (spaSocket.on == switchToOn):
+  print('No need to switch state')
+else:
+  spaSocket.on = not spaSocket.on
+  print('Tried to switch state')
+
+
+
